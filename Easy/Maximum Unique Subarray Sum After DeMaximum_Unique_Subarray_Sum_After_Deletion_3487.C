65227126a57fec6905e3d72ecaf342a57b51871b
shiva kumar durgam <shivayadav0539@gmail.com>
2025-07-25 16:57:16 +0530

    25th july leetcode problem

---
+++ Easy/Maximum Unique Subarray Sum After DeMaximum_Unique_Subarray_Sum_After_Deletion_3487.C
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        if max(nums)<0 :
+            return max(nums)
+        else:
+            result=0
+            nums=set(nums)
+            for i in nums:
+                if i>=0:
+                    result+=i
+            return result
